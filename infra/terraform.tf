5d3c3588f377b4977145387d71cfb3847ec34095
Jumpbox <adeolu@jumpbox.jumpbox.local>
2024-09-09 23:50:11 +0100

    Ansible Assignment x AWS

---
+++ infra/terraform.tf
@@ -0,0 +1,11 @@
+# Configuration file
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.62.0"
+    }
+  }
+
+  required_version = ">= 1.9.4"
+}
